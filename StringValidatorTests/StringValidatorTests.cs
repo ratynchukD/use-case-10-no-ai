49fffce32cced33e8497fa3257fb8f954ad91e16
Dmytro Ratynchuk <ratynchuk@gmail.com>
2023-08-27 18:46:20 +0300

    UC#10 Implement regex tests

---
+++ StringValidatorTests/StringValidatorTests.cs
@@ -0,0 +1,60 @@
+using StringUtilities;
+
+namespace StringValidatorTests
+{
+	public class StringValidatorTests
+	{
+		[Theory]
+		[InlineData(null, false)]
+		[InlineData("", false)]
+		[InlineData(" ", false)]
+		[InlineData("	", false)]
+		[InlineData("123", false)]
+		[InlineData("Abc@", false)]
+		[InlineData("Ab12345!Ab12345!Ab12345!", false)]
+		[InlineData("12345", false)]
+		[InlineData("1234@", false)]
+		[InlineData("A12345%", false)]
+		[InlineData("Ab12 345^", false)]
+		[InlineData("Ab12	345^", false)]
+		[InlineData("Ab12\n345^", false)]
+		[InlineData("Ab12345!", true)]
+		[InlineData("Ab12345\"", true)]
+		[InlineData("Ab12345#", true)]
+		[InlineData("Ab12345$", true)]
+		[InlineData("Ab12345%", true)]
+		[InlineData("Ab12345&", true)]
+		[InlineData("Ab12345'", true)]
+		[InlineData("Ab12345(", true)]
+		[InlineData("Ab12345)", true)]
+		[InlineData("Ab12345*", true)]
+		[InlineData("Ab12345+", true)]
+		[InlineData("Ab12345,", true)]
+		[InlineData("Ab12345-", true)]
+		[InlineData("Ab12345.", true)]
+		[InlineData("Ab12345/", true)]
+		[InlineData("Ab12345:", true)]
+		[InlineData("Ab12345;", true)]
+		[InlineData("Ab12345<", true)]
+		[InlineData("Ab12345=", true)]
+		[InlineData("Ab12345>", true)]
+		[InlineData("Ab12345?", true)]
+		[InlineData("Ab12345@", true)]
+		[InlineData("Ab12345[", true)]
+		[InlineData("Ab12345\\", true)]
+		[InlineData("Ab12345]", true)]
+		[InlineData("Ab12345^", true)]
+		[InlineData("Ab12345_", true)]
+		[InlineData("Ab12345`", true)]
+		[InlineData("Ab12345{", true)]
+		[InlineData("Ab12345|", true)]
+		[InlineData("Ab12345}", true)]
+		[InlineData("Ab12345~", true)]
+		public void ValidatePassword_ReturnsCorrectResult(string? input, bool expectetResult)
+		{
+			var result = StringValidator.ValidatePassword(input);
+
+			Assert.Equal(expectetResult, result);
+		}
+	}
+}
