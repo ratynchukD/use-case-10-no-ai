ba92c4c5fcef7a7a0409e3dd6aca29cb515ef142
Dmytro Ratynchuk <ratynchuk@gmail.com>
2023-08-27 18:26:09 +0300

    UC#10 Implement regex

---
+++ StringValidator/StringValidator.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace StringUtilities
+{
+	
+	public static class StringValidator
+	{
+		private static Regex passwordRegex = new("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z])^[\\S]*$");
+
+		/// <summary>
+		/// Validates if a string contains at least one uppercase letter,
+		/// one lowercase letter, one digit, and one special character. 
+		/// It also validates if the string length is within the acceptable range.
+		/// </summary>
+		/// <param name="input">A string to be validated.</param>
+		/// <param name="minLength">The minimum required length.</param>
+		/// <param name="maxLength">The maximum required length.</param>
+		/// <returns></returns>
+		public static bool ValidatePassword(string? input, int minLength = 4, int maxLength = 12)
+		{
+			if (!string.IsNullOrWhiteSpace(input) 
+				&& input.Length >= minLength 
+				&& input.Length <= maxLength)
+			{
+				return passwordRegex.Match(input).Success;
+			}			
+
+			return false;
+		}
+	}
+}
---
+++ StringValidatorUsage/Program.cs
@@ -0,0 +1,57 @@
+﻿using StringUtilities;
+
+Console.WriteLine("StringValidator usage example");
+
+string?[] passwords = {
+	null,
+	"",
+	" ",
+	"	",
+	"123", // too short
+	"Ab12345!Ab12345!Ab12345!", // too long
+	"12345",
+	"1234@",
+	"A12345%",
+	"Ab12 345^",
+	"Ab12	345^",
+	"Ab12\n345^",
+	"Ab12345!",
+	"Ab12345\"",
+	"Ab12345#",
+	"Ab12345$",
+	"Ab12345%",
+	"Ab12345&",
+	"Ab12345'",
+	"Ab12345(",
+	"Ab12345)",
+	"Ab12345*",
+	"Ab12345+",
+	"Ab12345,",
+	"Ab12345-",
+	"Ab12345.",
+	"Ab12345/",
+	"Ab12345:",
+	"Ab12345;",
+	"Ab12345<",
+	"Ab12345=",
+	"Ab12345>",
+	"Ab12345?",
+	"Ab12345@",
+	"Ab12345[",
+	"Ab12345\\",
+	"Ab12345]",
+	"Ab12345^",
+	"Ab12345_",
+	"Ab12345`",
+	"Ab12345{",
+	"Ab12345|",
+	"Ab12345}",
+	"Ab12345~",
+};
+
+foreach (var password in passwords)
+{
+    Console.WriteLine($"Password: {password}. Is valid: {StringValidator.ValidatePassword(password)}");
+}
+
+Console.ReadLine();
